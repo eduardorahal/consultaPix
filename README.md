ddc3bb8fbe4000b9a14fd633d611f9af3d56f87c
eduardorahal <eduardorahal@gmail.com>
2023-11-23 10:04:09 -0300

    first commit

--- README.md
+++ README.md
@@ -1,36 +1,14 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Aplicação desenvolvida utilizando NEXT
 
-## Getting Started
+Após clone do código, comandar 'npm install'
 
-First, run the development server:
+Para rodar a aplicação, 'npm run dev' ou equivalente
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+Necessário configurar Imagens e Cabeçalho dos relatórios, que utiliza ferramenta PDFMAKE
+    IMAGENS:
+        Em /public/base64 alterar a logo da PC
+    
+    CABEÇALHO
+        Nos arquivos contidos em /relatorios/pix => Linhas 95 e 96
+        Atenção: Linhas 7 e 8 importam as imagens inseridas em /public/base64. Necessário verificar os nomes dos arquivos. Imagens são utilizadas nas linhas 92 e 99 (relatorio resumido) e 132 e 139 (relatorio detalhado). Necessário verificar os nomes também.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ app/api/bacen/pix/cpfCnpj/route.js
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    let lista = [];
+    let cpfCnpj = searchParams.get('cpfCnpj');
+    let motivo = searchParams.get('motivo');
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://www3.bcb.gov.br/bc_ccs/rest/consultar-vinculos-pix?cpfCnpj=' + cpfCnpj + '&motivo=' + motivo,
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Basic ZWp1ZnMucy1hcGljY3M6Ym9rYTIxMjM=',
+        }
+    };
+
+    const vinculos = await axios.request(config)
+        .then(res1 => res1.data.vinculosPix)
+        .then(async (chaves) => {
+            if (chaves.length > 0) {
+
+                // Ordena as chaves PIX recebidas para que as primeiras contenham CPF e Nome, preferencialmente
+
+                await chaves.sort((a, b) => a.cpfCnpj && a.nomeProprietario ? -1 : (a.cpfCnpj ? -1 : 1))
+
+                // Replica as informações de CPF e Nome para as demais chaves que não tenham tais informações
+
+                let nomeProprietarioBusca = chaves[0].nomeProprietario;
+                let cpfCnpjBusca = cpfCnpj;
+
+                for await (let chave of chaves) {
+                    await axios.get('https://www3.bcb.gov.br/informes/rest/pessoasJuridicas?cnpj=' + chave.participante)
+                        .then(response => response.data)
+                        .then((participante) => {
+                            chave.numerobanco = (participante.codigoCompensacao ? participante.codigoCompensacao.toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false }) : '000');
+                            chave.nomebanco = participante.nome;
+                            chave.cpfCnpjBusca = cpfCnpjBusca;
+                            chave.nomeProprietarioBusca = nomeProprietarioBusca;
+                        })
+                        .catch((err) => {
+                            chave.numerobanco = "000";
+                            chave.nomebanco = "BANCO NÃO INFORMADO";
+                            chave.cpfCnpjBusca = cpfCnpjBusca;
+                            chave.nomeProprietarioBusca = nomeProprietarioBusca;
+                        })
+                    for await (let evento of chave.eventosVinculo) {
+                        await axios.get('https://www3.bcb.gov.br/informes/rest/pessoasJuridicas?cnpj=' + evento.participante)
+                            .then(response => response.data)
+                            .then((participante) => {
+                                evento.numerobanco = (participante.codigoCompensacao ? participante.codigoCompensacao.toLocaleString('en-US', { minimumIntegerDigits: 3, useGrouping: false }) : '000');
+                                evento.nomebanco = participante.nome
+                            })
+                            .catch((err) => {
+                                evento.numerobanco = "000";
+                                evento.nomebanco = "BANCO NÃO INFORMADO"
+                            })
+                    }
+                    if (chave.status == null) {
+                        chave.status = 'INATIVO'
+                    }
+                }
+                lista.push(chaves)
+            } else {
+                lista.push(chaves)
+            }
+        })
+        .catch((error) => {
+            lista.push(error.response.data.message)
+        })
+
+    return NextResponse.json(lista)
+}
